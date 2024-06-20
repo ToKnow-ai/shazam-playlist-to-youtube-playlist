c8ea80c809f5f685598972a1200a4cbb9f9dec93
Kabue Charles <mckabue@gmail.com>
2024-06-20 22:26:00 +0300

    works

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    try:
+        return 'Hello, World!'
+    except Exception as e:
+        return str(e)
 
-@app.route('/video_id/<video_id>')
-def get_video_id(video_id):
-    return f'video_id = {video_id}!'
+@app.route('/video_id', methods=['POST'])
+def get_video_id():
+    try:
+        title = request.json.get('title')
+        artist = request.json.get('artist')
+        return { 'artist': artist, 'title': title }
+    except Exception as e:
+        return str(e)
