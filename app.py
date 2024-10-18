2f4074b2533f944b63e06819265e7d177ad3f63d
Kabue Charles <mckabue@gmail.com>
2024-10-18 13:04:20 +0300

    Add CSV parsing utility and test route; update UI for loading test playlist

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 """Shazam Playlist to Youtube Playlist"""
 
+from pathlib import Path
 from typing import Optional
 import logging
 import pandas as pd
@@ -37,9 +38,17 @@ def parse_csv():
     try:
         file = request.files['file']
         # Process the uploaded file
-        shazamlibrary_df = pd.read_csv(file, header=1)
-        shazamlibrary_df = shazamlibrary_df.drop_duplicates(subset=['TrackKey'])[['Title', 'Artist']]
-        return shazamlibrary_df.to_json(orient="records")
+        return parse_csv_util(pd.read_csv(file, header=1))
+    except Exception as e:
+        return str(e)
+
+@app.route('/parse_csv_test', methods=['GET'])
+def parse_csv_test():
+    """Route handler for parsing the test CSV file"""
+    try:
+        # Construct the path to the CSV file
+        csv_path = Path(__file__).parent / 'shazamlibrary.test.csv'
+        return parse_csv_util(pd.read_csv(csv_path, header=1))
     except Exception as e:
         return str(e)
 
@@ -47,3 +56,10 @@ def get_youtube_song(title: str, artist: str) -> Optional[YouTube]:
     """Searches for a YouTube video based on the given title and artist"""
     search_result = Search(f'{title} by {artist}')
     return search_result.results[0] if search_result.results else None
+
+def parse_csv_util(shazamlibrary_df):
+    try:
+        shazamlibrary_df = shazamlibrary_df.drop_duplicates(subset=['TrackKey'])[['Title', 'Artist']]
+        return shazamlibrary_df.to_json(orient="records")
+    except Exception as e:
+        return str(e)
